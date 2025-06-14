30073bb2f3d236c2af870796cdafda3b7f220080
Divyesh Mali <medivyeshmali@gmail.com>
2025-06-15 01:05:32 +0530

    gluestack-ui setup done successfully

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import "./global.css"
+import { GluestackUIProvider } from './src/components/ui/gluestack-ui-provider';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider>
+        <Text>Hello</Text>
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
